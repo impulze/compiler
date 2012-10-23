5f5b3cc24e81700b191f9e59de3ea33667dc44b7
Daniel Mierswa <impulze@impulze.org>
2012-10-23 17:32:32 +0200

    parser_debug gets an ANTLR_STRING

--- src/boofar_parser_debug.h
+++ src/boofar_parser_debug.h
@@ -2,6 +2,6 @@
 #define BOOFAR_PARSER_DEBUG_H_INCLUDED
 
 /* this file is used by the parser grammer, hence no C++ allowed */
-void boofar_parser_debug(char const *string);
+void boofar_parser_debug(ANTLR_STRING *);
 
 #endif
