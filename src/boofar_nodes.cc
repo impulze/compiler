f91084dbcdfc9a6db8f5987dbf2c42ba107cacb6
Maximilian Lasser <max.lasser@online.de>
2012-11-06 10:34:46 +0100

    changed design of nodes::generic
    added the first few simple node types

--- src/boofar_nodes.cc
+++ src/boofar_nodes.cc
@@ -1,14 +1,48 @@
+#include <sstream>
 #include "boofar_nodes.h"
 
 namespace boofar
 {
 	namespace nodes
 	{
-		identifier::identifier(std::string name):
-			name(name)
+		// define type names
+		// auto generic::type_names TODO report clang bug
+		decltype(generic::type_names) generic::type_names
+		{
+			{ types::type::dec_literal, "decimal literal" },
+			{ types::type::identifier, "identifier" }
+		};
+
+		// generic
+		generic::generic(types::type type):
+			type(type)
 		{}
 
-		type identifier::get_type() const
-		{ return type::identifier; }
+		std::string generic::to_string() const
+		{
+			return (std::ostringstream() << '<' << type_names[type] << ':' <<
+				get_string_value() << '>').str();
+		}
+
+		// identifier
+		identifier::identifier(const std::string &name):
+			generic(types::type::identifier), name(name)
+		{}
+
+		std::string identifier::get_string_value() const
+		{ return name; }
+
+		// literal
+		literal::literal(types::type type, const std::string &constructor):
+			generic(type), constructor(constructor)
+		{}
+
+		std::string literal::get_string_value() const
+		{ return constructor; }
+
+		// decimal_literal
+		decimal_literal::decimal_literal(const std::string &constructor):
+			literal(types::type::dec_literal, constructor)
+		{}
 	};
 };
--- src/boofar_nodes.h
+++ src/boofar_nodes.h
@@ -1,29 +1,53 @@
 #ifndef _BOOFAR_NODES_H_
 #define _BOOFAR_NODES_H_
 
+#include <unordered_map>
 #include "boofar.h"
 
 namespace boofar
 {
 	namespace nodes
 	{
+
 		class generic
 		{
 		public:
-			virtual types::type get_type() const = 0;
-			virtual std::string to_string() const = 0;
+			types::type get_type() const;
+			std::string to_string() const;
+
+		protected:
+			generic(types::type type);
 
 		private:
-			std::string build_string(std::string value) const;
+			static std::unordered_map<int, std::string> type_names;
+			const types::type type;
+			virtual std::string get_string_value() const = 0;
 		};
 
 		class identifier : generic
 		{
+		public:
+			identifier(const std::string &name);
+
+		private:
 			std::string name;
+			std::string get_string_value() const override;
+		};
 
-			identifier(std::string name);
+		class literal : generic
+		{
+		protected:
+			literal(types::type type, const std::string &constructor);
 
-			type get_type() const override;
+		private:
+			std::string constructor;
+			std::string get_string_value() const override;
+		};
+
+		class decimal_literal : literal
+		{
+		public:
+			decimal_literal(const std::string &constructor);
 		};
 	};
 };
